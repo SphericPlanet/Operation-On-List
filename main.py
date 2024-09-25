1c88153bd99d4bc79b27a235563d409e4f5b41f4
SphericPlanet <gauranshpandey09@gmail.com>
2024-09-25 18:39:44 +0530

    Created Operation On List

---
+++ main.py
@@ -0,0 +1,26 @@
+alist = ["Student 1", 122, 3.45, "School Student"]
+print(alist)
+
+car_companies = ["Honda", "Toyota", "BMW", "Ford", "Tesla"]
+
+print("First Element:", car_companies[0])
+print("Second Element:", car_companies[1])
+print("Last Element:", car_companies[-1])
+print("List Size:", len(car_companies))
+
+car_companies.append("Chevrolet")
+print("After adding:", car_companies)
+
+car_companies.sort()
+print("After sorting in asc order:", car_companies)
+
+car_companies.reverse()
+print("After sorting in desc order:", car_companies)
+
+print("Multiply List Elements:",car_companies * 2)
+
+print("Slicing:",car_companies[ :6])
+
+car_companies.clear()
+print("Clearing the list:",car_companies )
+
